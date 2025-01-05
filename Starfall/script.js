337df045d9620f38eaf7595b5db8ceff76639266
Dali777777 <agent.l.official13@gmail.com>
2025-01-05 21:39:58 +0700

    Add files via upload

---
+++ Starfall/script.js
@@ -0,0 +1,22 @@
+
+
+
+
+// Переменная для хранения баланса
+let Balance = 0;
+
+// Получаем элементы
+const showMessageBtn = document.getElementById("showMessageBtn");
+const balanceValue = document.getElementById("balanceValue");
+
+// Функция для увеличения баланса на 1
+function increaseBalance() {
+    Balance += 1; // Прибавляем 1 к балансу
+    balanceValue.textContent = Balance; // Обновляем отображение баланса на странице
+}
+
+// Добавляем обработчик события на кнопку
+showMessageBtn.addEventListener("click", increaseBalance);
+
+
+
